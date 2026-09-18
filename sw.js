dce4f304b35da8e3cfeb737e743fa830bebab091
yutateru6-collab <yutateru6@gmail.com>
2026-09-18 09:00:13 +0900

    Cache SVG card artwork

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 const CACHE="genkai-karuta-v1";
 const ASSETS=[
   "./","./index.html","./styles.css","./cards.js","./app.js","./manifest.webmanifest",
-  "./assets/a.webp","./assets/ki.webp","./assets/so.webp","./assets/su.webp","./assets/wa.webp"
+  "./assets/a.svg","./assets/ki.svg","./assets/so.svg","./assets/su.svg","./assets/wa.svg"
 ];
 self.addEventListener("install",(e)=>e.waitUntil(caches.open(CACHE).then((c)=>c.addAll(ASSETS))));
 self.addEventListener("activate",(e)=>e.waitUntil(caches.keys().then((keys)=>Promise.all(keys.filter((k)=>k!==CACHE).map((k)=>caches.delete(k))))));
